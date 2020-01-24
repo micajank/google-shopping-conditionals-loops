d5eecd8622bd1c5b3bd7fd28c33485573e035cdf
micajank <micajank@umich.edu>
2020-01-23 21:17:34 -0800

    Submitting homework_2

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,8 +2,9 @@
 
 Please describe your comfort and completeness levels before submitting.
 
-Comfort Level (1-5): 
+Comfort Level (1-5): 5
 
-Completeness Level (1-5):
+Completeness Level (1-5): 5
 
 What did you think of this deliverable?:
+I found this deliverable to be quite simple, altho I had a bit of trouble understanding the instruction specifics. Like if I should only print out products who's author name was ONLY "eBay" or if an extended author name was okay. In that instance I just did both.
--- js/google-shopping.js
+++ js/google-shopping.js
@@ -16,25 +16,61 @@ console.log(data.items[0])
 
 console.log('\nProblem 1:\n')
 
+var count = 0;
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].kind === "shopping#product") {
+        count++;
+    }
+}
+console.log(count);
+
 // 2.) Print the `title` all items with a `backorder` availability
 // in `inventories`.
 
 console.log('\nProblem 2:\n')
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].product["inventories"][0]["availability"] === "backorder") {
+        console.log(data.items[i].product["title"], "\n");
+    }
+}
+
 
 // 3.) Print the `title` all items with more than one image link.
 
 console.log('\nProblem 3:\n')
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].product["images"].length > 1) {
+        console.log(data.items[i].product["title"], "\n");
+    }
+}
 
 // 4.) Print all "Canon" products in the items
 // HINT: careful with case sensitivity!
 
 console.log('\nProblem 4:\n')
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].product["brand"] === "Canon") {
+        console.log(data.items[i].product, "\n");
+    }
+}
 
 // 5.) Print all `items` that have an author name of "eBay" and are
 // brand "Canon".
 // HINT: What is the type of author?
 
 console.log('\nProblem 5:\n')
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].product["author"].name === "eBay" && data.items[i].product["brand"] === "Canon") {
+        console.log(data.items[i]);
+    }
+}
+console.log("------------Alternative Version (Name is ONLY eBay and has no extended name -------------");
+for (let i = 0; i < data.items.length; i++) {
+    if (data.items[i].product["author"].name === "eBay" && data.items[i].product["brand"] === "Canon" && data.items[i].product["author"].name.length === 4) {
+        console.log(data.items[i]);
+    }
+}
+
 
 // 6.) Print all the products with their **brand**, **price**,
 // and an **image link**
@@ -42,3 +78,9 @@ console.log('\nProblem 5:\n')
 // and inventories arrays.
 
 console.log('\nProblem 6:\n')
+for (let i = 0; i < data.items.length; i++) {
+    console.log(data.items[i].product["brand"]);
+    console.log(data.items[i].product["inventories"][0].price);
+    console.log(data.items[i].product["images"][0].link,  " \n");
+}
+
